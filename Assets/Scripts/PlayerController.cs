6e4f1f5789506329d0673a56e4d230a98892ca63
Ritesh <ritesh.kawadkar@gmail.com>
2016-07-28 17:17:19 +0530

    Added Jump to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ public class PlayerController : MonoBehaviour {
 
 	public float jumpSpeed = 500f;
 	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
 
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -16,5 +18,7 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButtonUp("Jump")){
 			myRigidBody.AddForce(transform.up * jumpSpeed);
 		}
+
+		myAnim.SetFloat ("vVelocity", Mathf.Abs(myRigidBody.velocity.y));
 	}
 }
