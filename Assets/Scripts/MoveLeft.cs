9d72d229a458deaf2fdec253c22c5e09aab1dd38
Ritesh <ritesh.kawadkar@gmail.com>
2016-07-30 02:54:18 +0530

    Multiple Updates
    1. Enemy Spooning
    2. Player Death
    3. Level Rest

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeft : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//deltaTime = time used to com,plete last update
+		transform.position += Vector3.left * speed * (Time.deltaTime);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,46 @@ public class PlayerController : MonoBehaviour {
 	public float jumpSpeed = 500f;
 	private Rigidbody2D myRigidBody;
 	private Animator myAnim;
+	private Collider2D myCollider;
 
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D> ();
 		myAnim = GetComponent<Animator> ();
+		myCollider = GetComponent<Collider2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonUp("Jump")){
 			myRigidBody.AddForce(transform.up * jumpSpeed);
 		}
-
+		//setFloat method is used to increase th evVelocity component as we have made a 
+		//transiion on value of vVelocity
 		myAnim.SetFloat ("vVelocity", Mathf.Abs(myRigidBody.velocity.y));
 	}
+
+
+
+	//when something collides
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.layer == LayerMask.NameToLayer ("Enemy")) {
+			//Application.LoadLevel(Application.loadedLevel);
+			foreach(PrefabSpooner objPrefab in FindObjectsOfType<PrefabSpooner>()){
+				objPrefab.enabled=false;
+			}
+			foreach(MoveLeft objMoveLeft in FindObjectsOfType<MoveLeft>()){
+				objMoveLeft.enabled=false;
+			}
+			myAnim.SetBool("PlayerKilled", true);
+
+			myRigidBody.velocity = Vector2.zero;
+			myRigidBody.AddForce(transform.up * jumpSpeed);
+			myCollider.enabled = false;
+		}
+
+
+
+	}
+
 }
