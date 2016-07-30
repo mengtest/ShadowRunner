247a234e8adbcd4500f5f3a58a4ab84585a1c724
Ritesh <ritesh.kawadkar@gmail.com>
2016-07-30 05:31:17 +0530

    completion
    Parallax
    Scoreboard
    Android setup
    Audio

---
+++ Assets/Recycler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recycler : MonoBehaviour {
+
+	public Transform startPoint;
+	public Transform endPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < endPoint.position.x) {
+			float gap = endPoint.position.x - transform.position.x;
+			transform.position = new Vector3(startPoint.position.x - gap, startPoint.position.y, startPoint.position.z); 
+		}
+	
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour {
 
 	public float jumpSpeed = 500f;
+	public AudioSource jumpSfx;
+	public AudioSource deathSfx;
+	public Text ScoreText;
+
 	private Rigidbody2D myRigidBody;
 	private Animator myAnim;
 	private Collider2D myCollider;
+	private float startTime;
 
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D> ();
 		myAnim = GetComponent<Animator> ();
 		myCollider = GetComponent<Collider2D> ();
+
+		startTime = Time.time;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonUp("Jump")){
+		ScoreText.text = (Time.time - startTime).ToString ("0.0");
+		if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Jump")){
 			myRigidBody.AddForce(transform.up * jumpSpeed);
+			jumpSfx.Play();
 		}
 		//setFloat method is used to increase th evVelocity component as we have made a 
 		//transiion on value of vVelocity
@@ -30,7 +41,7 @@ public class PlayerController : MonoBehaviour {
 	//when something collides
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.layer == LayerMask.NameToLayer ("Enemy")) {
-			//Application.LoadLevel(Application.loadedLevel);
+
 			foreach(PrefabSpooner objPrefab in FindObjectsOfType<PrefabSpooner>()){
 				objPrefab.enabled=false;
 			}
@@ -40,8 +51,12 @@ public class PlayerController : MonoBehaviour {
 			myAnim.SetBool("PlayerKilled", true);
 
 			myRigidBody.velocity = Vector2.zero;
+			deathSfx.Play();
 			myRigidBody.AddForce(transform.up * jumpSpeed);
 			myCollider.enabled = false;
+
+			Application.LoadLevel(Application.loadedLevel);
+
 		}
 
 
